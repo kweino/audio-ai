d207866af97aa7577f76290b416e4d95439a86a3
Kevin Weingarten <weingarten.kevin@gmail.com>
2022-12-13 13:20:37 -0800

    added pre-processing to standardize audio sample durations

--- urbansounddataset.py
+++ urbansounddataset.py
@@ -8,11 +8,17 @@ import torchaudio
 
 class UrbanSoundDataset(Dataset):
 
-    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate):
+    def __init__(self, 
+                 annotations_file, 
+                 audio_dir, 
+                 transformation, 
+                 target_sample_rate,
+                 num_samples):
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir
         self.transformation = transformation
         self.target_sample_rate = target_sample_rate
+        self.num_samples = num_samples
 
     def __len__(self):
         return len(self.annotations)
@@ -23,8 +29,12 @@ class UrbanSoundDataset(Dataset):
         signal, sr = torchaudio.load(audio_sample_path) 
         signal = self._resample_if_necessary(signal, sr) # standardize sample rate
         signal = self._mix_down_if_necessary(signal) # mix down to mono
-        signal = self.transformation(signal) # apply whatever transformation passed in
+        signal = self._right_pad_if_necessary(signal) # zero pad undersampled audio
+        signal = self._cut_if_necessary(signal) # trim sounds longer than target sr
+        signal = self.transformation(signal) # apply passed-in transformation 
         return signal, label
+    
+    
 
     def _get_audio_sample_path(self, index):
         fold = f'fold{self.annotations.iloc[index, 5]}'
@@ -33,6 +43,12 @@ class UrbanSoundDataset(Dataset):
     
     def _get_audi_sample_label(self, index):
         return self.annotations.iloc[index, 6]
+    
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
 
     def _mix_down_if_necessary(self, signal):
         '''signal -> (num_channels, samples). Transforms (2, 16000) -> (1, 16000)'''
@@ -40,18 +56,26 @@ class UrbanSoundDataset(Dataset):
             signal = torch.mean(signal, dim=0, keepdim=True)
         return signal
 
-    def _resample_if_necessary(self, signal, sr):
-        if sr != self.target_sample_rate:
-            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
-            signal = resampler(signal)
+    def _cut_if_necessary(self,signal):
+        # signal -> Tensor(1, num_samples)
+        if signal.shape[1] > self.num_samples:
+            signal = signal[:,:self.num_samples]
         return signal
 
+    def _right_pad_if_necessary(self, signal):
+        signal_length = signal.shape[1]
+        if signal_length < self.num_samples:
+            num_missing_samples = self.num_samples - signal_length
+            last_dim_padding = (0,num_missing_samples) # (right pad, left pad)
+            signal = torch.nn.functional.pad(signal,last_dim_padding)
+        return signal
 
 if __name__ == 'main':
     ANNOTATIONS_FILE = os.getenv('ANNOTATIONS_FILE')
     AUDIO_DIR = os.getenv('AUDIO_DIR')
-    SAMPLE_RATE = 16000
-
+    SAMPLE_RATE = 22050
+    NUM_SAMPLES = 22050
+    print(ANNOTATIONS_FILE)
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE,
         n_fft=1024,
@@ -61,9 +85,11 @@ if __name__ == 'main':
 
     #ms = mel_spectrogram(signal)
 
-    usd = UrbanSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, 
-                            transformation=mel_spectrogram, 
-                            target_sample_rate=SAMPLE_RATE)
+    usd = UrbanSoundDataset(ANNOTATIONS_FILE, 
+                            AUDIO_DIR, 
+                            mel_spectrogram, 
+                            SAMPLE_RATE,
+                            NUM_SAMPLES)
 
     print(f'There are {len(usd)} samples in the dataset')
 
